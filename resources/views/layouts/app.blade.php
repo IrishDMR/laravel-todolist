f1e6695ade3862d21331f9b7e8aacb35ff43bf72
David <davidmurphyreeve@gmail.com>
2021-09-26 17:54:55 +0100

    Todos index page

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>TodoList</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+<div class="container">
+    @yield('content')
+</div>
+
+<footer id="footer" class="text-center">
+    <p>Copyright &copy; 2021 TodoList</p>
+</footer>
+</body>
+</html>
