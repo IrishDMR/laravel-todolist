ecdc0f413266b1b96a0d35a10332626ce779ffec
David <davidmurphyreeve@gmail.com>
2021-09-28 10:38:07 +0100

    Show page and includes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,12 +5,14 @@
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-<div class="container">
-    @yield('content')
-</div>
+    @include('inc.navbar')
+    <div class="container">
+        @include('inc.messages')
+        @yield('content')
+    </div>
 
-<footer id="footer" class="text-center">
-    <p>Copyright &copy; 2021 TodoList</p>
-</footer>
+    <footer id="footer" class="text-center">
+        <p>Copyright &copy; 2021 TodoList</p>
+    </footer>
 </body>
 </html>
